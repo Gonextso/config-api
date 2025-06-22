0570c1dfa6e72f7796f0bc0b81e41c99328d312a
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-06-22 22:52:54 +0300

    tenant change

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 import './builders/envBuilder.js';
 import './builders/apiBuilder.js';
 import './builders/customBuilder.js';
+
+//TODO: control startDate and make sure it is not bigger 1 year from now
