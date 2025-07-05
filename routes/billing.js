a70c4265e4308808a0b09e8be369b139dc66f051
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-05 19:43:32 +0300

    fix: router

--- routes/billing.js
+++ routes/billing.js
@@ -4,7 +4,7 @@ import BillingController from "../controllers/BillingController.js";
 const router = express.Router();
 
 router.get("/plans", BillingController.getPlans);
-router.get("/subscribe", BillingController.subscribeToPlan);
+router.post("/subscribe", BillingController.subscribeToPlan);
 router.get("/confirm", BillingController.confirmCallback);
 
 export default router;
