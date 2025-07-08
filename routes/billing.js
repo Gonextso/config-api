22be9aacbe894ca0357485f4c4a5d475682b7f6c
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-08 17:56:30 +0300

    auth changed on billing

--- routes/billing.js
+++ routes/billing.js
@@ -1,10 +1,11 @@
 import express from "express";
 import BillingController from "../controllers/BillingController.js";
+import AuthMiddleware from "../middlewares/AuthMiddleware.js";
 
 const router = express.Router();
 
 router.get("/plans", BillingController.getPlans);
-router.post("/subscribe", BillingController.subscribeToPlan);
+router.post("/subscribe", AuthMiddleware.isShopifyAuthenticated, BillingController.subscribeToPlan);
 router.get("/confirm", BillingController.confirmCallback);
 
 export default router;
