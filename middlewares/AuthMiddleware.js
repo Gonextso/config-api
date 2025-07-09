6a06c2be3541ad69b38beb6ccf18a965ff864654
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-09 14:13:14 +0300

    tryout

--- middlewares/AuthMiddleware.js
+++ middlewares/AuthMiddleware.js
@@ -123,7 +123,7 @@ export default new class AuthMiddleware extends CoreController {
             bodyString = rawBody;
         }
 
-        console.log(JSON.stringify(req.body), '----', hmacHeader);
+        console.log(JSON.stringify(req.body), '----', bodyString);
         const generatedHmac = CryptoHelper.createShopifyWebhookHmac(bodyString);
         console.log(generatedHmac, '----', hmacHeader);
         if (generatedHmac !== hmacHeader) {
