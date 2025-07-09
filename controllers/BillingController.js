a1baf52b33f82894601505c7d2689de4a018b37a
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-09 13:28:39 +0300

    fix: subscription id added

--- controllers/BillingController.js
+++ controllers/BillingController.js
@@ -9,7 +9,7 @@ export default new class BillingController extends CoreController {
     }
 
     handleSubscriptionUpdate = async (req, res) => {
-        const { status, plan_handle, admin_graphql_api_shop_id } = req.body.app_subscription;
+        const { status, plan_handle, admin_graphql_api_shop_id, admin_graphql_api_id } = req.body.app_subscription;
         const tenant = await Tenant.findOne({ "shopify.shopId": admin_graphql_api_shop_id.replace("gid://shopify/Shop/", "") });
 
         if (!tenant) return this.response(res, { status: HttpStatusCodes.NOT_FOUND });
@@ -17,6 +17,7 @@ export default new class BillingController extends CoreController {
         if (status.toLowerCase() === "active" || status.toLowerCase() === "trialing") {
             tenant.updateOne({ 
                 "shopify.billing.planKey": plan_handle,
+                "shopify.billing.subscription.id": admin_graphql_api_id,
                 "shopify.billing.tokenLimit": SystemCodes.BILLING_PLANS[plan_handle].TOKEN_LIMIT,
                 "shopify.billing.tokenUsed": 0,
                 "shopify.billing.periodStart": new Date().toISOString(),
