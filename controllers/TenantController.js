116604bd507cbc36475255f4e6354390330d655b
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2025-07-12 15:48:05 +0300

    log changed

--- controllers/TenantController.js
+++ controllers/TenantController.js
@@ -54,8 +54,8 @@ export default new class TenantController extends CoreController {
             });
 
         ObjectHelper.deepMerge(tenant, updateData);
-
-        console.log(tenant);
+            
+        this.logger.info2(`Tenant ${tenant.name} updated with data ${JSON.stringify(updateData)}`);
 
         await tenant.save();
 
