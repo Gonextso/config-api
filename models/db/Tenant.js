9dc71a4acdf8fd7ac9fb492f2cbf1d7a04afffa7
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2026-01-08 13:11:04 +0300

    fix: update

--- models/db/Tenant.js
+++ models/db/Tenant.js
@@ -91,7 +91,7 @@ export default mongoose.model('Tenant', new mongoose.Schema({
                         isActive: { type: Boolean, default: false }
                     },
                     status: {
-                        interval: { type: String, default: "0 0 * * *" },
+                        interval: { type: String, default: "*/5 * * * *" },
                         startDate: {
                             type: String,
                             default: function () {
