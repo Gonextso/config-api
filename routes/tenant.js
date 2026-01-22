a1e83cd2cac1cfe48663c03ed06463b1c05f1234
Efe Murat Rendeci <efemuratrendeci@hotmail.com>
2026-01-22 11:58:24 +0300

    feat: status

--- routes/tenant.js
+++ routes/tenant.js
@@ -3,6 +3,7 @@ import TenantController from "../controllers/TenantController.js";
 
 const router = express.Router();
 
+router.get("/sync_status", TenantController.getSyncStatus);
 router.get("/", TenantController.getTenant);
 router.patch("/", TenantController.patchTenant);
 router.delete("/", TenantController.deleteTenant);
